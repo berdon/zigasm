db8e5c2e4f9b34db4ee777b643fda047e0886465
Austin Hanson <austin.hanson@oracle.com>
2023-10-09 13:33:46 -0500

    Initial commit; args pulled in

---
+++ src/main.zig
@@ -0,0 +1,10 @@
+const std = @import("std");
+
+pub fn main() anyerror!void {
+    var allocator = std.heap.page_allocator;
+
+    const args = try std.process.argsAlloc(allocator);
+    defer std.process.argsFree(allocator, args);
+
+    std.debug.print("Arguments: {s}\n", .{args});
+}
